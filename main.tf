02f8d704e827511d620b00e875e737608e0b6e0f
qbart <wojtowicz.bartlomiej@gmail.com>
2023-06-23 12:56:23 +0200

    Drop depreacted domain verification, allow to verify DKIM via module

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+locals {
+  dkim_verification_attrs = [for dkim in aws_ses_domain_dkim.this.dkim_tokens : {
+    name  = "${dkim}._domainkey.${var.domain_name}"
+    ttl   = 600
+    type  = "CNAME"
+    value = "${dkim}.dkim.amazonses.com"
+  }]
+}
+
 resource "aws_ses_email_identity" "this" {
   for_each = var.email_addresses
   email    = each.key
@@ -27,4 +36,15 @@ data "aws_iam_policy_document" "this" {
 resource "aws_iam_policy" "this" {
   name   = "${var.name_prefix}-ses-send-email"
   policy = data.aws_iam_policy_document.this.json
+}
+
+
+resource "aws_route53_record" "this_verify_dkim" {
+  count = var.zone_id != "" && var.verify_dkim ? length(local.dkim_verification_attrs) : 0
+
+  zone_id = var.zone_id
+  name    = local.dkim_verification_attrs[count.index].name
+  type    = local.dkim_verification_attrs[count.index].type
+  ttl     = local.dkim_verification_attrs[count.index].ttl
+  records = [local.dkim_verification_attrs[count.index].value]
 }
--- variables.tf
+++ variables.tf
@@ -8,8 +8,22 @@ variable "domain_name" {
   description = "The domain name from which AWS SES will be able to send emails."
 }
 
+# optional
+
 variable "email_addresses" {
   type        = set(string)
   default     = []
   description = "Emails from which AWS SES will be able to send emails."
+}
+
+variable "zone_id" {
+  type        = string
+  description = "The Route53 zone ID for the domain name."
+  default     = ""
+}
+
+variable "verify_dkim" {
+  type        = bool
+  description = "Automatically verify DKIM records in Route53."
+  default     = false
 }
